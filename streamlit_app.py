a0430fbba778607518645c903e4bfc2d53a57355
dalepres <dale@dalepreston.com>
2022-10-29 20:49:50 -0500

    Update streamlit_app.py
    More wasted time learning emojis instead of Snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit
 
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
-streamlit.text('\N{oatmeal}Omega 3 & Blueberry Oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('\N{kale}Kale, Spinach & Rocket Smoothie')
 streamlit.text('\N{chicken}Hard-Boiled Free-Range Egg')
 streamlit.text('\N{banana}\N{strawberry}Build Your Own Fruit Smoothie\N{kiwi|\N{grapes}
