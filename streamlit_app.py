f0b4ebda29ba8c10d5dfed7f282f5cd0da84f749
dalepres <dale@dalepreston.com>
2022-10-30 00:05:58 -0500

    Update streamlit_app.py
    Missed removing a broken emoji.  They're all gone now.

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
-streamlit.text('Build Your Own Fruit Smoothie\N{kiwi|}
+streamlit.text('Build Your Own Fruit Smoothie')
 
                
 my_fruit_list = pandas.readcsv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
