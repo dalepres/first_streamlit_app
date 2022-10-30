338823f4d32e57eddc24c7b47c56c8af83dbbfd1
dalepres <dale@dalepreston.com>
2022-10-29 20:37:41 -0500

    Update streamlit_app.py
    trying emoji

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('\N{banana}Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 streamlit.text('Hard-Boiled Free-Range Egg')
