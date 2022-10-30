d04f4094bf971f073025fc54d7de0348f8276278
dalepres <dale@dalepreston.com>
2022-10-30 00:04:33 -0500

    Update streamlit_app.py
    Added pandas and fruits file.  Deleted emojis to focus on learning Snowflake

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit
+import pandas
 
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('\N{kale}Kale, Spinach & Rocket Smoothie')
-streamlit.text('\N{chicken}Hard-Boiled Free-Range Egg')
-streamlit.text('\N{banana}\N{strawberry}Build Your Own Fruit Smoothie\N{kiwi|\N{grapes}
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('Build Your Own Fruit Smoothie\N{kiwi|}
+
+               
+my_fruit_list = pandas.readcsv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
