8cb9826e2ad4c07d3549a4d9c1ecb08702131e6a
dalepres <dale@dalepreston.com>
2022-10-30 21:54:47 -0500

    Update streamlit_app.py
    Cleaning up by following instructions in the lab

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,9 @@
 import streamlit
-import pandas
-import requests
-import snowflake.connector
+# import pandas
+# import requests
+# import snowflake.connector
+from urllib.error import URLError
+
 
 streamlit.title('My Mom\'s New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -35,6 +37,7 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
