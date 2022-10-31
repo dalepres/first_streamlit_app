54f84383427afba2341efe76739ae1beaf01a520
dalepres <dale@dalepreston.com>
2022-10-30 22:34:34 -0500

    Update streamlit_app.py
    typo on 63 if

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ def insert_row_snowflake(new_fruit):
     return "Thanks for adding " + new_fruit
   
 add_my_fruit = streamlit.text_input('What fruit would you like to add?')
-if (streamlit.button('Add a Fruit to the List'):
+if streamlit.button('Add a Fruit to the List'):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
   fruit_insert_result = insert_row_snowflake(add_my_fruit)
   streamlit.text(fruit_insert_result)
